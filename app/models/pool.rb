992c99169db312f7af542482aff2431a0bd68f84
sebasking01 <noordinarypeopleallowed@gmail.com>
2015-04-08 11:11:10 -0400

    fixed conflicts

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -4,7 +4,8 @@ class Pool < ActiveRecord::Base
   has_many :invitations
   validates :name, presence: true
   validates :pool_amount, presence: true
-  # accepts_nested_attributes_for :invitations
+  accepts_nested_attributes_for :invitations, reject_if: proc { |attributes| attributes['guest'].blank? }
+
 
 
   def liferaft_amount
