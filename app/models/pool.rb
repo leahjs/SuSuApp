fd4d79b834cfdbadc6264c9f46cb339e76cf28c2
sebasking01 <noordinarypeopleallowed@gmail.com>
2015-04-09 14:19:46 -0400

    added lifeguards to pool for conditional rendering of invites

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,12 +1,12 @@
 class Pool < ActiveRecord::Base
-  has_one :lifeguard, class_name: :user
+  belongs_to :lifeguard, class_name: 'User'
   has_many :swimmers
   has_many :invitations
   validates :name, presence: true
   validates :pool_amount, presence: true
   accepts_nested_attributes_for :invitations, reject_if: proc { |attributes| attributes['guest'].blank? }
 
-# binding.pry
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408145802) do
+ActiveRecord::Schema.define(version: 20150409180808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 20150408145802) do
     t.integer  "member_limit"
     t.string   "status"
     t.integer  "reciever"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "invitation_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "lifeguard_id"
   end
 
   create_table "swimmers", force: :cascade do |t|
