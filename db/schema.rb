e7215b9eae1a1e58c7d594d1e773afffb7dde562
leahjs <leahj.suter@gmail.com>
2015-06-29 01:06:53 -0400

    added stripeid to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413005143) do
+ActiveRecord::Schema.define(version: 20150628195858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20150413005143) do
     t.string   "provider"
     t.string   "uid"
     t.string   "profile_photo"
+    t.string   "stripeid"
   end
 
   add_foreign_key "invitations", "pools"
